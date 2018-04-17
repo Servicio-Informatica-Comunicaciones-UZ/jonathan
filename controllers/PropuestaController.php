e1b55ef2a65cd384d230f45c6c97f8a4ac591c6b
Enrique Matías Sánchez (Quique) <cronopios@gmail.com>
2018-04-17 12:46:23 +0200

    Evitar error si no se rellena la macroárea

--- controllers/PropuestaController.php
+++ controllers/PropuestaController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use app\models\Propuesta;
 use app\models\PropuestaCentro;
 use app\models\PropuestaMacroarea;
+use Yii;
 
 /**
  * This is the class for controller "PropuestaController".
@@ -16,25 +17,29 @@ class PropuestaController extends \app\controllers\base\PropuestaController
         $model = new Propuesta();
 
         try {
-            if ($model->load($_POST) && $model->save()) {
-                $macroareas = $_POST['Propuesta']['propuestaMacroareas'];
-                foreach ($macroareas as $macroarea) {
-                    $pm = new PropuestaMacroarea();
-                    $pm->propuesta_id = $model->id;
-                    $pm->macroarea_id = $macroarea;
-                    $pm->save();
+            if ($model->load(Yii::$app->request->post()) && $model->save()) {
+                $macroareas = Yii::$app->request->post('Propuesta')['propuestaMacroareas'];
+                if ($macroareas) {
+                    foreach ($macroareas as $macroarea) {
+                        $pm = new PropuestaMacroarea();
+                        $pm->propuesta_id = $model->id;
+                        $pm->macroarea_id = $macroarea;
+                        $pm->save();
+                    }
                 }
-                $centros = $_POST['Propuesta']['propuestaCentros'];
+                $centros = Yii::$app->request->post('Propuesta')['propuestaCentros'];
                 foreach ($centros as $centro) {
-                    $pc = new PropuestaCentro();
-                    $pc->propuesta_id = $model->id;
-                    $pc->nombre_centro = $centro;
-                    $pc->save();
+                    if ($centro) {
+                        $pc = new PropuestaCentro();
+                        $pc->propuesta_id = $model->id;
+                        $pc->nombre_centro = $centro;
+                        $pc->save();
+                    }
                 }
 
                 return $this->redirect(['view', 'id' => $model->id]);
             } elseif (!\Yii::$app->request->isPost) {
-                $model->load($_GET);
+                $model->load(Yii::$app->request->get());
             }
         } catch (\Exception $e) {
             $msg = (isset($e->errorInfo[2])) ? $e->errorInfo[2] : $e->getMessage();
