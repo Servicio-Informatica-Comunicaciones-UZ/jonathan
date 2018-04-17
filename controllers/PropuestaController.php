8cab757521d95ed0833d69956e201ef8cb790539
Enrique Matías Sánchez (Quique) <cronopios@gmail.com>
2018-04-17 10:52:17 +0200

    Primera versión del formulario para crear propuestas

--- controllers/PropuestaController.php
+++ controllers/PropuestaController.php
@@ -2,10 +2,45 @@
 
 namespace app\controllers;
 
+use app\models\Propuesta;
+use app\models\PropuestaCentro;
+use app\models\PropuestaMacroarea;
+
 /**
-* This is the class for controller "PropuestaController".
-*/
+ * This is the class for controller "PropuestaController".
+ */
 class PropuestaController extends \app\controllers\base\PropuestaController
 {
+    public function actionCrear()
+    {
+        $model = new Propuesta();
 
+        try {
+            if ($model->load($_POST) && $model->save()) {
+                $macroareas = $_POST['Propuesta']['propuestaMacroareas'];
+                foreach ($macroareas as $macroarea) {
+                    $pm = new PropuestaMacroarea();
+                    $pm->propuesta_id = $model->id;
+                    $pm->macroarea_id = $macroarea;
+                    $pm->save();
+                }
+                $centros = $_POST['Propuesta']['propuestaCentros'];
+                foreach ($centros as $centro) {
+                    $pc = new PropuestaCentro();
+                    $pc->propuesta_id = $model->id;
+                    $pc->nombre_centro = $centro;
+                    $pc->save();
+                }
+
+                return $this->redirect(['view', 'id' => $model->id]);
+            } elseif (!\Yii::$app->request->isPost) {
+                $model->load($_GET);
+            }
+        } catch (\Exception $e) {
+            $msg = (isset($e->errorInfo[2])) ? $e->errorInfo[2] : $e->getMessage();
+            $model->addError('_exception', $msg);
+        }
+
+        return $this->render('crear', ['model' => $model]);
+    }
 }
---
+++ views/propuesta/crear.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+
+$this->title = Yii::t('models', 'Propuesta');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('models', 'Propuestas'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+
+<h1>
+    <?php echo Yii::t('models', 'Propuesta'); ?>
+    <small>
+        <?php echo $model->id; ?>
+    </small>
+</h1>
+<hr><br>
+
+<div class="clearfix crud-navigation">
+    <div class="pull-left">
+        <?php
+        echo Html::a(
+            Yii::t('jonathan', 'Cancelar'),
+            \yii\helpers\Url::previous(),
+            ['class' => 'btn btn-default']
+        );
+        ?>
+    </div>
+</div>
+
+<hr />
+
+<?php echo $this->render('_formulario', ['model' => $model]); ?>
