0a935cc9aa4a392dd3354bd19350134207aea7f1
Enrique Matías Sánchez (Quique) <quique@unizar.es>
2018-06-04 15:30:19 +0200

    fix Mostrar mensaje Flash si se ha establecido con setFlash

--- views/site/_flashes.php
+++ views/site/_flashes.php
@@ -9,7 +9,11 @@ use yii\bootstrap\Alert;
 use yii\helpers\Html;
 
 if (Yii::$app->session->hasFlash('error')) {
-    foreach (Yii::$app->session->getFlash('error') as $msg) {
+    $flashes = Yii::$app->session->getFlash('error');
+    if (!is_array($flashes)) {  // Flash establecido mediante setFlash() y no addFlash()
+        $flashes = [$flashes];
+    }
+    foreach ($flashes as $msg) {
         echo Alert::widget([
             'body' => "<span class='glyphicon glyphicon-remove-sign'></span>" . nl2br(Html::encode($msg)),
             'options' => ['class' => 'alert-danger'],
@@ -18,7 +22,11 @@ if (Yii::$app->session->hasFlash('error')) {
 }
 
 if (Yii::$app->session->hasFlash('warning')) {
-    foreach (Yii::$app->session->getFlash('warning') as $msg) {
+    $flashes = Yii::$app->session->getFlash('warning');
+    if (!is_array($flashes)) {  // Flash establecido mediante setFlash() y no addFlash()
+        $flashes = [$flashes];
+    }
+    foreach ($flashes as $msg) {
         echo Alert::widget([
             'body' => "<span class='glyphicon glyphicon-exclamation-sign'></span>" . nl2br(Html::encode($msg)),
             'options' => ['class' => 'alert-warning'],
@@ -27,7 +35,11 @@ if (Yii::$app->session->hasFlash('warning')) {
 }
 
 if (Yii::$app->session->hasFlash('info')) {
-    foreach (Yii::$app->session->getFlash('info') as $msg) {
+    $flashes = Yii::$app->session->getFlash('info');
+    if (!is_array($flashes)) {  // Flash establecido mediante setFlash() y no addFlash()
+        $flashes = [$flashes];
+    }
+    foreach ($flashes as $msg) {
         echo Alert::widget([
             'body' => "<span class='glyphicon glyphicon-info-sign'></span>" . nl2br(Html::encode($msg)),
             'options' => ['class' => 'alert-info'],
@@ -36,7 +48,11 @@ if (Yii::$app->session->hasFlash('info')) {
 }
 
 if (Yii::$app->session->hasFlash('success')) {
-    foreach (Yii::$app->session->getFlash('success') as $msg) {
+    $flashes = Yii::$app->session->getFlash('success');
+    if (!is_array($flashes)) {  // Flash establecido mediante setFlash() y no addFlash()
+        $flashes = [$flashes];
+    }
+    foreach ($flashes as $msg) {
         echo Alert::widget([
             'body' => "<span class='glyphicon glyphicon-ok-sign'></span>" . nl2br(Html::encode($msg)),
             'options' => ['class' => 'alert-success'],
@@ -46,7 +62,11 @@ if (Yii::$app->session->hasFlash('success')) {
 
 // Si tenemos la seguridad de que el mensaje es seguro, podemos no codificarlo.
 if (Yii::$app->session->hasFlash('safe-success')) {
-    foreach (Yii::$app->session->getFlash('safe-success') as $msg) {
+    $flashes = Yii::$app->session->getFlash('safe-success');
+    if (!is_array($flashes)) {  // Flash establecido mediante setFlash() y no addFlash()
+        $flashes = [$flashes];
+    }
+    foreach ($flashes as $msg) {
         echo Alert::widget([
             'body' => "<span class='glyphicon glyphicon-ok-sign'></span>" . nl2br($msg),
             'options' => ['class' => 'alert-success'],
