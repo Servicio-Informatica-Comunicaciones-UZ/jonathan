13e9a64317f5d3bf23b75018f12909cf94275a0a
Enrique Matías Sánchez (Quique) <quique@unizar.es>
2019-07-11 16:05:52 +0200

    fix Usar HtmlPurifier en vez de Html::encode

--- views/evaluador/valoracion/crear.php
+++ views/evaluador/valoracion/crear.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\HtmlPurifier;
 
 $this->title = Yii::t('evaluador', 'Nueva evaluación');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('models', 'Propuestas asignadas'), 'url' => ['evaluador/index']];
@@ -20,7 +21,43 @@ $this->registerCssFile('@web/css/gestion.css', ['depends' => 'app\assets\AppAsse
 
 <h2><?php echo Html::encode($model->respuesta->pregunta->titulo);?></h2>
 <p class='pregunta'><strong><?php echo nl2br(Html::encode($model->respuesta->pregunta->descripcion)); ?></strong></p>
-<p><?php echo nl2br(Html::encode($model->respuesta->valor)); ?></p>
+
+<?php
+echo '<div>' . str_replace(
+    '<table>',
+    "<table class='table table-bordered'>",
+    HtmlPurifier::process(
+        $model->respuesta->valor,
+        [
+        'Attr.ForbiddenClasses' => ['Apple-interchange-newline', 'Apple-converted-space',
+            'Apple-paste-as-quotation', 'Apple-style-span', 'Apple-tab-span', 'BalloonTextChar', 'BodyTextIndentChar',
+            'Heading1Char', 'Heading2Char', 'Heading3Char', 'Heading4Char', 'Heading5Char', 'Heading6Char',
+            'IntenseQuoteChar', 'MsoAcetate', 'MsoBodyText', 'MsoBodyText1', 'MsoBodyText2', 'MsoBodyText3',
+            'MsoBodyTextIndent', 'MsoBookTitle', 'MsoCaption', 'MsoChpDefault',
+            'MsoFooter', 'MsoHeader', 'MsoHyperlink', 'MsoHyperlinkFollowed',
+            'MsoIntenseEmphasis', 'MsoIntenseQuote', 'MsoIntenseReference', 'MsoListParagraph',
+            'MsoListParagraphCxSpFirst', 'MsoListParagraphCxSpMiddle', 'MsoListParagraphCxSpLast',
+            'MsoNormal', 'MsoNormalTable', 'MsoNoSpacing', 'MsoPapDefault', 'MsoQuote',
+            'MsoSubtleEmphasis', 'MsoSubtleReference', 'MsoTableGrid',
+            'MsoTitle', 'MsoTitleCxSpFirst', 'MsoTitleCxSpMiddle', 'MsoTitleCxSpLast',
+            'MsoToc1', 'MsoToc2', 'MsoToc3', 'MsoToc4', 'MsoToc5', 'MsoToc6', 'MsoToc7', 'MsoToc8', 'MsoToc9',
+            'MsoTocHeading', 'QuoteChar', 'SubtitleChar', 'TitleChar',
+            'western', 'WordSection1', ],
+        'CSS.ForbiddenProperties' => ['background', 'border', 'border-bottom',
+            'border-collapse', 'border-left', 'border-right', 'border-style', 'border-top', 'border-width',
+            'font', 'font-family', 'font-size', 'font-weight', 'height', 'line-height',
+            'margin', 'margin-bottom', 'margin-left', 'margin-right', 'margin-top',
+            'padding', 'padding-bottom', 'padding-left', 'padding-right', 'padding-top',
+            'text-autospace', 'text-indent', 'width', ],
+        'HTML.ForbiddenAttributes' => ['align', 'background', 'bgcolor', 'border',
+            'cellpadding', 'cellspacing', 'height', 'hspace', 'noshade', 'nowrap',
+            'rules', 'size', 'valign', 'vspace', 'width', ],
+        'HTML.ForbiddenElements' => ['font'],
+        // 'data' permite incrustar imágenes en base64
+        'URI.AllowedSchemes' => ['data' => true, 'http' => true, 'https' => true, 'mailto' => true],
+        ]
+    )
+) . "</div>\n\n"; ?>
 
 <div class='cuadro-gris'>
     <h3><?php echo "{$model->bloque->titulo} ({$model->bloque->porcentaje}%)"; ?></h3>
